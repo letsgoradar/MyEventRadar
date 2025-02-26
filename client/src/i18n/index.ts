5298d5de28e1ddaafb74b0e10783ca9acbb92695
bruijnnieuwendi <39866032-bruijnnieuwendi@users.noreply.replit.com>
2025-02-26 15:43:49 +0000

    Restored to '732669fcae1ba119b12af66ed42351324780b81e'
    Replit-Restored-To:732669fcae1ba119b12af66ed42351324780b81e

--- client/src/i18n/index.ts
+++ client/src/i18n/index.ts
@@ -1,23 +1,14 @@
+
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import { translations } from './translations';
-import LanguageDetector from 'i18next-browser-languagedetector';
 
 i18n
-  .use(LanguageDetector)
   .use(initReactI18next)
   .init({
     resources: translations,
     lng: 'nl', // default language
     fallbackLng: 'en',
-    detection: {
-      order: ['navigator', 'querystring', 'cookie', 'localStorage', 'sessionStorage', 'htmlTag'],
-      lookupQuerystring: 'lng',
-      lookupCookie: 'i18next',
-      lookupLocalStorage: 'i18nextLng',
-      lookupSessionStorage: 'i18nextLng',
-      caches: ['localStorage', 'cookie'],
-    },
     interpolation: {
       escapeValue: false
     }
